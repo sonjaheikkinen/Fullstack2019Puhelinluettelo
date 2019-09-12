c86e12f2776c481cd46982540c2252d51340684f
Heikkinen Sonja T <heisonja@cubbli18-gold.local>
2019-09-12 17:53:04 +0300

    Tehtäviä numeroon 3.12 asti

---
+++ mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+const password = process.argv[2]
+const url =
+`mongodb+srv://sonja:${password}@cluster0-evjkv.mongodb.net/phonebook?retryWrites=true&w=majority`
+mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if ( process.argv.length<3 ) {
+
+  console.log('give password as argument')
+  process.exit(1)
+
+} else if (process.argv.length === 3) {
+
+  console.log('phonebook:')
+
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person.name, person.number)
+    })
+    mongoose.connection.close()
+  })
+
+} else {
+
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  })
+  
+  person.save().then(response => {
+    console.log(`added ${process.argv[3]} number ${process.argv[4]} to phonebook`);
+    mongoose.connection.close();
+  })
+
+}
+
