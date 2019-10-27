828ae968dfe81a64c7c299cf01b4a6f056e3b4ce
Heikkinen Sonja T <heisonja@cubbli18-gold.local>
2019-10-27 23:01:57 +0200

    muutoksia

---
+++ build/precache-manifest.51598c227fca76dd375a89fb882ceba3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf36b706e90a3a71db44709cd507852a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3c43942679f58e2553a",
+    "url": "/static/css/main.d24de726.chunk.css"
+  },
+  {
+    "revision": "edb70b9760340e495c07",
+    "url": "/static/js/2.42c16a5d.chunk.js"
+  },
+  {
+    "revision": "c3c43942679f58e2553a",
+    "url": "/static/js/main.ba2a72e4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
